7b61d811a3c15efb92d49838a15eae840a6fa4b4
Code Whisperer <daniel@herald.to>
2023-02-23 13:49:27 -0500

    :virgo:

--- src/Charts/Bar.jsx
+++ src/Charts/Bar.jsx
@@ -1,62 +1,62 @@
-// export function BarChart({ data }) {
+// // export function BarChart({ data }) {
 
-//   const margin = { top: 0, right: 0, bottom: 0, left: 0 };
-//   const width = 500 - margin.left - margin.right;
-//   const height = 300 - margin.top - margin.bottom;
+// //   const margin = { top: 0, right: 0, bottom: 0, left: 0 };
+// //   const width = 500 - margin.left - margin.right;
+// //   const height = 300 - margin.top - margin.bottom;
 
-//   const scaleX = scaleBand()
-//     // .domain(data.map(({ label }) => label))
-//     .domain(data)
-//     .range([0, width]);
-//   return (
+// //   const scaleX = scaleBand()
+// //     // .domain(data.map(({ label }) => label))
+// //     .domain(data)
+// //     .range([0, width]);
+// //   return (
     
-//     <svg
-//       width={width + margin.left + margin.right}
-//       height={height + margin.top + margin.bottom}>
-//       <g transform={`translate(${margin.left}, ${margin.top})`}></g>
-//     </svg>
-//   );
+// //     <svg
+// //       width={width + margin.left + margin.right}
+// //       height={height + margin.top + margin.bottom}>
+// //       <g transform={`translate(${margin.left}, ${margin.top})`}></g>
+// //     </svg>
+// //   );
 
+// // }
+
+// import React from 'react'
+// import Paper from '@mui/material/Paper';
+// import {
+//   Chart,
+//   BarSeries,
+//   Title,
+//   ArgumentAxis,
+//   ValueAxis,
+// } from '@devexpress/dx-react-chart-material-ui';
+
+// import { Animation } from '@devexpress/dx-react-chart';
+
+// export class BarChart extends React.PureComponent {
+//   constructor(props) {
+//     super(props);
+
+//     // this.state = {
+//     //   data : props.data
+//     // };
+//   }
+
+//   render() {
+//     // const { data: chartData } = this.state;
+
+//     return (
+//       <Paper>
+//         <Chart data={this.props.data} >
+//           {/* <ArgumentAxis /> */}
+//           {/* <ValueAxis max={7} /> */}
+
+//           <BarSeries
+//             valueField="population"
+//             argumentField="year"
+//           />
+//           <Title text="World population" />
+//           <Animation />
+//         </Chart>
+//       </Paper>
+//     );
+//   }
 // }
-
-import React from 'react'
-import Paper from '@mui/material/Paper';
-import {
-  Chart,
-  BarSeries,
-  Title,
-  ArgumentAxis,
-  ValueAxis,
-} from '@devexpress/dx-react-chart-material-ui';
-
-import { Animation } from '@devexpress/dx-react-chart';
-
-export class BarChart extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    // this.state = {
-    //   data : props.data
-    // };
-  }
-
-  render() {
-    // const { data: chartData } = this.state;
-
-    return (
-      <Paper>
-        <Chart data={this.props.data} >
-          {/* <ArgumentAxis /> */}
-          {/* <ValueAxis max={7} /> */}
-
-          <BarSeries
-            valueField="population"
-            argumentField="year"
-          />
-          <Title text="World population" />
-          <Animation />
-        </Chart>
-      </Paper>
-    );
-  }
-}
